62320643444a913a09813b130545587249a3dd1e
Lionel <lionel.prat9@gmail.com>
2021-07-27 15:35:23 +0200

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ ! -d db ]; then
+  echo "First start: docker-compose -f docker-compose_guacamole.yml up -d"
+  exit
+fi
+docker run --rm lprat/guacamole-client /opt/guacamole/bin/initdb.sh --postgres > ./db/initdb.sql
+docker-compose exec postgres createdb -U guacamole guacamole_db
+docker-compose exec postgres psql -U guacamole -d guacamole_db -f /db/initdb.sql
+chown -R 1000.1000 guac-record
