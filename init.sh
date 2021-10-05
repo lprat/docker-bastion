1b3243c17c33545b1fce81d57848031d0e48997f
Lionel <lionel.prat9@gmail.com>
2021-10-05 15:48:36 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,5 +6,5 @@ fi
 docker run --rm lprat/guacamole-client /opt/guacamole/bin/initdb.sh --postgres > ./db/initdb.sql
 docker-compose exec postgres createdb -U guacamole guacamole_db
 docker-compose exec postgres psql -U guacamole -d guacamole_db -f /db/initdb.sql
-chown -R 1000.1000 guac-record
-chown -R 1000.1000 guac-share_rdp
+chown -R 1000.1000 `grep 'LOCAL_PATH_RECORD' .env |awk -F '=' '{print $2}'`
+chown -R 1000.1000 `grep 'LOCAL_PATH_RDP' .env |awk -F '=' '{print $2}'`
