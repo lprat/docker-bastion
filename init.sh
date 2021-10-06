e0b47e016f7fb298ca584ca6a832a5831932097d
Lionel <lionel.prat9@gmail.com>
2021-10-06 17:24:04 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,3 +8,6 @@ docker-compose exec postgres createdb -U guacamole guacamole_db
 docker-compose exec postgres psql -U guacamole -d guacamole_db -f /db/initdb.sql
 chown -R 1000.1000 `grep 'LOCAL_PATH_RECORD' .env |awk -F '=' '{print $2}'`
 chown -R 1000.1000 `grep 'LOCAL_PATH_RDP' .env |awk -F '=' '{print $2}'`
+docker cp guacamole:/home/guacamole/tomcat/conf/server.xml server.xml
+sed -i 's#</Host>#<Valve className="org.apache.catalina.valves.RemoteIpValve" internalProxies="127.0.0.1" remoteIpHeader="x-forwarded-for" remoteIpProxiesHeader="x-forwarded-by" protocolHeader="x-forwarded-proto" />#g' server.xml
+echo "restart guacamole: docker-compose -f docker-compose_guacamole.yml guacamole"
